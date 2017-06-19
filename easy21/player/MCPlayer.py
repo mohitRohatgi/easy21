aaa89a77305272efa67665716fa5bee8f40246b3
Mohit Rohatgi <mohit.rohatgi@walmart.com>
2017-06-19 14:46:32 +0530

    added update and end episode, plot based on dealers showing remaining

--- easy21/player/MCPlayer.py
+++ easy21/player/MCPlayer.py
@@ -16,63 +16,84 @@ if __name__ == "__main__" and __package__ is None:
 
 class MCPlayer(Player):
     def __init__(self):
-        self.player_total = self._get_first_card()
-        self.dealer = Dealer()
-        self.dealer_showing = self.dealer.dealer_total
         self.action_state = np.zeros((10, 2))
         self.times_state_visited = np.zeros(10)
-        self.times_state_action_visited = np.zeros((10, 2))
+        self.state_visited = []
+        self.action_taken = []
         self.N0 = 100.0
-        self.step_size = self._get_step_size()
-        self.states_action_visited = []
     
     def learn(self):
-        for i in range(1):
+        for i in range(10000):
+            self._start_episode()
             reward = self._perform_episode()
             self._update_action_state(reward)
             self._end_episode()
+        print (self.action_state)
+    
+    def _start_episode(self):
+        self.player_total = self._get_first_card()
+        self.dealer = Dealer()
+        self.dealer_showing = self.dealer.dealer_total
     
     def _perform_episode(self):
         self.eps = self._get_eps()
         reward = None
         while (reward == None):
             state_idx = self._get_state_idx()
-            self.times_state_visited[state_idx] += 1
             action = self._get_action_idx(state_idx)
-            self.states_action_visited.append([state_idx, action])
             reward = self._perform_action(action)
         return reward
     
-    def _perform_action(self, action_idx):
-        if (action_idx == 0):
-            return self._stick()
-        return self._hit()
-    
     def _update_action_state(self, reward):
-        self.action_state[self.states_action_visited] += reward
+        error = self._get_error(reward)
+        step_size = self._get_step_size()
+        self.action_state[self.state_visited, self.action_taken] += error * step_size
     
     def _end_episode(self):
-        pass
+        self.state_visited = []
+        self.action_taken = []
+    
+    def _get_error(self, reward):
+        return reward - self.action_state[self.state_visited, self.action_taken]
+    
+    def _get_eps(self):
+        return self.N0 / (self.N0 + self.times_state_visited)
+    
+    def _get_state_idx(self):
+        while (self.player_total < 12):
+            self._hit()
+        state_idx = self.player_total - 12
+        self.state_visited.append(state_idx)
+        self.times_state_visited[state_idx] += 1
+        return state_idx
     
     # assuming 0 to stick and 1 to be hit index
     def _get_action_idx(self, state_idx):
         max_arg = np.argmax(self.action_state[state_idx])
         prob = np.random.random()
         if prob > 1.0 - self.eps[state_idx] / len(self.action_state[state_idx]):
-            return max_arg
+            action = max_arg
         else:
-            return self._choose_random_action(max_arg)
+            action = self._choose_random_action(max_arg)
+        self.action_taken.append(action)
+        return action
     
-    def _get_state_idx(self):
-        while (self.player_total < 12):
-            self._hit()
-        return self.player_total - 12
+    def _perform_action(self, action_idx):
+        if (action_idx == 0):
+            return self._stick()
+        return self._hit()
     
     def _choose_random_action(self, max_arg):
         action = np.random.randint(0, len(self.action_state[0]))
         while (action == int(max_arg)):
             action = np.random.randint(0, len(self.action_state[0]))
         return action
+    
+    def _get_step_size(self):
+        return 1.0 / self.times_state_visited[self.state_visited]
+    
+    def _get_first_card(self):
+        return np.random.randint(1, 11)
     
     def _hit(self):
         self.player_total += self.dealer.hit()
@@ -85,15 +106,6 @@ class MCPlayer(Player):
         if self.dealer.dealer_total > 21.0:
             return 1.0
         return self._get_reward()
-    
-    def _get_first_card(self):
-        return np.random.randint(1, 11)
-    
-    def _get_eps(self):
-        return self.N0 / (self.N0 + self.times_state_visited)
-    
-    def _get_step_size(self):
-        return 1.0 / self.times_state_visited
     
     def _get_reward(self):
         if self.dealer.dealer_total > self.player_total:
